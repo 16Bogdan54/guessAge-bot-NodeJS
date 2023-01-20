5e2abf4504e673a88f17624510bade3b9859894b
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-01-20 08:58:31 +0200

    commands test

--- commands/command.js
+++ commands/command.js
@@ -4,3 +4,7 @@
 * for each command that the bot supports, such as /start and /help.
 *
 * */
+
+// bot.onText(/\/ask/, (message) => {
+//     bot.sendMessage(message.chat.id, 'Ask received')
+// })
--- index.js
+++ index.js
@@ -7,11 +7,33 @@
 **/
 
 import TelegramBot from 'node-telegram-bot-api';
+import env from 'dotenv';
 
-const bot = new TelegramBot('TOKEN', {polling: true})
+env.config()
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
+const bot = new TelegramBot(process.env.TOKEN, {polling: true})
 
-    bot.sendMessage(chatId, 'Received message')
+bot.on('message',async (message) => {
+    const text = message.text;
+    const id = message.chat.id;
+
+    switch (text) {
+        case '/start':
+            await bot.sendSticker(id, 'https://tlgrm.eu/_/stickers/a02/7f2/a027f2cb-1aa3-456c-910f-4d7245ba437c/1.webp')
+            await bot.sendMessage(id, 'Hi I can guess your age, just enter /start')
+            break;
+
+        case '/ask':
+            await bot.sendMessage(id, 'Your age is less or bigger than 50?')
+            break;
+
+        case '/changeLang':
+            await bot.sendMessage(id, 'Change language/Змінити мову/Изменить язык  /changeLang');
+            break;
+
+        default:
+            await bot.sendMessage(id, `Oops! You've entered wrong command or plain text`)
+            break;
+    }
+
 });
