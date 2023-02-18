ff1b871cfb58b289aab28670e34767217582b285
16Bogdan54 <Yunakov74@gmail.com>
2023-02-18 14:42:52 +0200

    log

--- message-handlers/languageHandler.js
+++ message-handlers/languageHandler.js
@@ -3,7 +3,7 @@ import i18n from "i18n";
 export const languageHandler = (bot) => {
   bot.on("callback_query", (msg) => {
     const lang = msg.data;
-    // console.log();
+    console.log();
     i18n.setLocale(lang);
   });
 };
