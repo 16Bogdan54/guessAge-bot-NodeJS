ebe4f38fa7679d08b1a72a9008d525f3fd657983
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-02-24 21:55:25 +0200

    translation

--- options/guessOptions.js
+++ options/guessOptions.js
@@ -4,9 +4,9 @@ export const guessOptions = {
   reply_markup: JSON.stringify({
     inline_keyboard: [
       [
-        { text: i18n.__("yes_btn"), callback_data: "1" },
-        { text: i18n.__("lower_btn"), callback_data: "2" },
-        { text: i18n.__("higher_btn"), callback_data: "3" },
+        { text: `${i18n.__("yes_btn")}`, callback_data: "1" },
+        { text: `${i18n.__("lower_btn")}`, callback_data: "2" },
+        { text: `${i18n.__("higher_btn")}`, callback_data: "3" },
       ],
     ],
   }),
